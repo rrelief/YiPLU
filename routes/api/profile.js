387941249acdd1b013abd2bbdcd156895e2a3c32
Rrelief <rxr261@case.edu>
2018-07-14 17:15:51 -0400

    route descriptions

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//@route   GET request for api/profile/test
+//@desc    Tests for  post route
+//@acess   Public
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//@route   GET request for api/users/test
+//@desc    Tests for  post route
+//@acess   Public
 router.get("/test", (req, res) => res.json({ msg: "User Works" }));
 
 module.exports = router;
